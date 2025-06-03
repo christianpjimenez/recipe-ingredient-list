7fe7a451b81130335fbcc6d61d52708fb0987736
Sebastian <68107565+sebb-pj@users.noreply.github.com>
2025-06-03 19:21:44 +0200

    backend changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); 
 require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
@@ -12,14 +13,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// API routes
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
 app.use('/api/ingredients', ingredientRoutes);
 app.use('/api/list', listRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
 
-// DB connection
+// DB connection + start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT, () => {
