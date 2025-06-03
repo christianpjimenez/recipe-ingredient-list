05e4c456c319869e87b2c1bb3aedec22a0031a2f
Sebastian <68107565+sebb-pj@users.noreply.github.com>
2025-06-03 20:25:11 +0200

    debugging backend, moved files order

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const cors = require('cors');
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path'); 
 require('dotenv').config();
