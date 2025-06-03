78bbcf724efe40ff3da492f65cf9c987636f0648
Sebastian <68107565+sebb-pj@users.noreply.github.com>
2025-06-03 19:58:49 +0200

    debugging backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path'); 
 require('dotenv').config();
 
