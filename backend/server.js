fbfd6809227db511919d25c1eaf3da5f2ba88a7a
Sebastian <68107565+sebb-pj@users.noreply.github.com>
2025-06-03 21:28:09 +0200

    added alternative port

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,8 @@ console.log('Express app initialized and static files served, fetching database 
 // DB connection + start server
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
     });
   })
   .catch(err => console.log('DB connection error:', err));
